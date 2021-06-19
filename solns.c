691b3d8d6e49cc4d3498777517d243b2223efd1e
SatyanshuVishwakarma <84718007+SatyanshuVishwakarma@users.noreply.github.com>
2021-06-19 07:41:04 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,14 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+#include<assert.h>
+
+int max(int x[],int count) {
+	int lar=x[0];
+	for(int i=0;i<count-1;i++) {
+		if(x[i+1]>lar)
+			lar=x[i+1];
+	}
+	return lar;
+}
+
+
