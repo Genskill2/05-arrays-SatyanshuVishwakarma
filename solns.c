41d45c47b16ebb6b2bd5a1043835a38393a18fd5
SatyanshuVishwakarma <84718007+SatyanshuVishwakarma@users.noreply.github.com>
2021-06-19 07:50:05 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -11,4 +11,11 @@ int max(int x[],int count) {
 	return lar;
 }
 
-
+int min(int x[],int count) {
+	int sml=x[0];
+	for(int i=0;i<count-1;i++) {
+		if(x[i+1]<sml)
+			sml=x[i+1];
+	}
+	return sml;
+}
